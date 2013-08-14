15a6629a26b59f927061e4586abb771fb0fdf7a4
Christian Joergensen <christian@technobabble.dk>
2013-08-14 15:12:43 +0200

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ubivox PHP API library
+ 
+Access the ![Ubivox API][1] from PHP.
+
+## License
+
+Unless otherwise specified in any source file, the code is put in the public domain.
+
+[1]: https://kb.ubivox.com/api/latest/html/
---
+++ ubivox_api.php
@@ -0,0 +1,85 @@
+<?php
+
+define("UBIVOX_API_CLIENT_VERSION", "0.1");
+
+require_once("IXR_library.php");
+
+class UbivoxAPIException extends Exception { }
+class UbivoxAPIUnavailable extends UbivoxAPIException { }
+class UbivoxAPIError extends UbivoxAPIException { }
+class UbivoxAPIUnauthorized extends UbivoxAPIException { }
+class UbivoxAPIConnectionError extends UbivoxAPIException { }
+class UbivoxAPINotFound extends UbivoxAPIException { }
+
+class UbivoxAPI {
+
+    function __construct($username, $password, $url) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->url = $url;
+    }
+
+    public function call($method, $params = null) {
+
+        $auth = $this->username.":".$this->password;
+
+        $request = new IXR_Request($method, $params);
+        $post = $request->xml;
+
+        $c = curl_init($this->url);
+
+        curl_setopt($c, CURLOPT_USERAGENT, "Ubivox API PHP client ". UBIVOX_API_CLIENT_VERSION)
+        curl_setopt($c, CURLOPT_POST, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($c, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($c, CURLOPT_USERPWD, $auth);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($c, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($c, CURLOPT_HEADER, true);
+        curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 5);
+
+        $http_response = curl_exec($c);
+
+        $info = curl_getinfo($c);
+
+        list($header, $data) = explode("\r\n\r\n", $http_response, 2);
+
+        if ($info["http_code"] == 200) {
+
+            # Support for 100 Continue HTTP header
+
+            if (strpos($data, "HTTP/1.1") == 0) {
+                list($header, $data) = explode("\r\n\r\n", $data, 2);
+            }
+
+            $message = new IXR_Message($data);
+            $message->parse();
+
+            if ($message->messageType == "fault") {
+                throw new UbivoxAPIError($message->faultString, $message->faultCode);
+            }
+
+            return $message->params[0];
+
+        }
+
+        if ($info["http_code"] == 401) {
+            throw new UbivoxAPIUnauthorized();
+        }
+
+        if ($info["http_code"] == 503) {
+            throw new UbivoxAPIUnavailable();
+        }
+
+        if ($info["http_code"] == 404) {
+            throw new UbivoxAPINotFound();
+        }
+
+        if ($info["http_code"] == 0) {
+            throw new UbivoxAPIConnectionError();
+        }
+
+        throw new UbivoxAPIException($header);
+
+    }
+}
